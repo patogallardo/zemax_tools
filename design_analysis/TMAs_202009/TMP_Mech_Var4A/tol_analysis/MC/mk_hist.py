3eddb6170db9d3477ad5f6023f8ab9a0cecc159a
patogallardo <26889221+patogallardo@users.noreply.github.com>
2021-02-01 10:00:50 -0500

    added tolerances and polarization

---
+++ design_analysis/TMAs_202009/TMP_Mech_Var4A/tol_analysis/MC/mk_hist.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+SHOW = False
+
+fname = 'sim_res.xls'
+df = pd.read_excel(fname)
+
+plt.hist(df["strehl"], bins=20, cumulative=False, density=False, histtype='step', color='black')
+plt.axvline(0.975 - 0.05, label='5% Strehl degradation')
+plt.xlabel('strehl[-]')
+plt.ylabel('counts [-]')
+plt.legend()
+
+plt.grid()
+
+
+if SHOW:
+    plt.show()
+else:
+    plt.savefig('hist_mc.png', dpi=120)
+    plt.close()
